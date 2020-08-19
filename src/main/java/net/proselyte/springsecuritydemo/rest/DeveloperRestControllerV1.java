8bf742118e35439180893011065af73ec4864959
Shvedic <melamoryblimm93@gmail.com>
2020-08-19 14:50:20 +0300

    Integration Spring Security. STEP2

--- src/main/java/net/proselyte/springsecuritydemo/rest/DeveloperRestControllerV1.java
+++ src/main/java/net/proselyte/springsecuritydemo/rest/DeveloperRestControllerV1.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.DecimalFormat;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
